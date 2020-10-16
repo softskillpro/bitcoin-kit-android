8d0badc508aac413ad748e0602680e0dda3b5670
softskillpro <softskill.pro20@gmail.com>
2020-10-16 02:10:06 +0000

    Add xcoin

---
+++ xcoinkit/src/main/kotlin/io/horizontalsystems/xcoinkit/XcoinKit.kt
@@ -0,0 +1,270 @@
+package io.horizontalsystems.xcoinkit
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import io.horizontalsystems.bitcoincore.AbstractKit
+import io.horizontalsystems.bitcoincore.BitcoinCore
+import io.horizontalsystems.bitcoincore.BitcoinCore.SyncMode
+import io.horizontalsystems.bitcoincore.BitcoinCoreBuilder
+import io.horizontalsystems.bitcoincore.blocks.validators.BlockValidatorChain
+import io.horizontalsystems.bitcoincore.blocks.validators.BlockValidatorSet
+import io.horizontalsystems.bitcoincore.blocks.validators.ProofOfWorkValidator
+import io.horizontalsystems.bitcoincore.extensions.hexToByteArray
+import io.horizontalsystems.bitcoincore.managers.*
+import io.horizontalsystems.bitcoincore.models.BalanceInfo
+import io.horizontalsystems.bitcoincore.models.BlockInfo
+import io.horizontalsystems.bitcoincore.models.TransactionInfo
+import io.horizontalsystems.bitcoincore.network.Network
+import io.horizontalsystems.bitcoincore.storage.CoreDatabase
+import io.horizontalsystems.bitcoincore.storage.Storage
+import io.horizontalsystems.bitcoincore.transactions.TransactionSizeCalculator
+import io.horizontalsystems.bitcoincore.utils.Base58AddressConverter
+import io.horizontalsystems.bitcoincore.utils.MerkleBranch
+import io.horizontalsystems.bitcoincore.utils.PaymentAddressParser
+import io.horizontalsystems.dashkit.IInstantTransactionDelegate
+import io.horizontalsystems.dashkit.InstantSend
+import io.horizontalsystems.dashkit.X11Hasher
+import io.horizontalsystems.dashkit.core.DashTransactionInfoConverter
+import io.horizontalsystems.dashkit.core.SingleSha256Hasher
+import io.horizontalsystems.dashkit.instantsend.*
+import io.horizontalsystems.dashkit.instantsend.instantsendlock.InstantSendLockHandler
+import io.horizontalsystems.dashkit.instantsend.instantsendlock.InstantSendLockManager
+import io.horizontalsystems.dashkit.instantsend.transactionlockvote.TransactionLockVoteHandler
+import io.horizontalsystems.dashkit.instantsend.transactionlockvote.TransactionLockVoteManager
+import io.horizontalsystems.dashkit.managers.*
+import io.horizontalsystems.dashkit.masternodelist.*
+import io.horizontalsystems.dashkit.messages.*
+import io.horizontalsystems.dashkit.models.CoinbaseTransactionSerializer
+import io.horizontalsystems.dashkit.models.DashTransactionInfo
+import io.horizontalsystems.dashkit.models.InstantTransactionState
+import io.horizontalsystems.dashkit.storage.DashKitDatabase
+import io.horizontalsystems.dashkit.storage.DashStorage
+import io.horizontalsystems.dashkit.tasks.PeerTaskFactory
+import io.horizontalsystems.dashkit.validators.DarkGravityWaveTestnetValidator
+import io.horizontalsystems.dashkit.validators.DarkGravityWaveValidator
+import io.horizontalsystems.hdwalletkit.Mnemonic
+import io.reactivex.Single
+
+class XcoinKit : AbstractKit, IInstantTransactionDelegate, BitcoinCore.Listener {
+    enum class NetworkType {
+        MainNet,
+        TestNet
+    }
+
+    interface Listener {
+        fun onTransactionsUpdate(inserted: List<DashTransactionInfo>, updated: List<DashTransactionInfo>)
+        fun onTransactionsDelete(hashes: List<String>)
+        fun onBalanceUpdate(balance: BalanceInfo)
+        fun onLastBlockInfoUpdate(blockInfo: BlockInfo)
+        fun onKitStateUpdate(state: BitcoinCore.KitState)
+    }
+
+    var listener: Listener? = null
+
+    override var bitcoinCore: BitcoinCore
+    override var network: Network
+
+    private val xcoinStorage: DashStorage
+    private val instantSend: InstantSend
+    private val xcoinTransactionInfoConverter: DashTransactionInfoConverter
+
+    constructor(
+            context: Context,
+            words: List<String>,
+            walletId: String,
+            networkType: NetworkType = NetworkType.MainNet,
+            peerSize: Int = 2,
+            syncMode: SyncMode = SyncMode.Full(),
+            confirmationsThreshold: Int = 6
+    ) : this(context, Mnemonic().toSeed(words), walletId, networkType, peerSize, syncMode, confirmationsThreshold)
+
+    constructor(
+            context: Context,
+            seed: ByteArray,
+            walletId: String,
+            networkType: NetworkType = NetworkType.MainNet,
+            peerSize: Int = 2,
+            syncMode: SyncMode = SyncMode.Full(),
+            confirmationsThreshold: Int = 6
+    ) {
+        val coreDatabase = CoreDatabase.getInstance(context, getDatabaseNameCore(networkType, walletId, syncMode))
+        val xcoinDatabase = DashKitDatabase.getInstance(context, getDatabaseName(networkType, walletId, syncMode))
+        val initialSyncUrl: String
+
+        val coreStorage = Storage(coreDatabase)
+        xcoinStorage = DashStorage(xcoinDatabase, coreStorage)
+
+        network = when (networkType) {
+            NetworkType.MainNet -> {
+                initialSyncUrl = ""
+                MainNetXcoin()
+            }
+            NetworkType.TestNet -> {
+                initialSyncUrl = ""
+                TestNetXcoin()
+            }
+        }
+
+        val paymentAddressParser = PaymentAddressParser("xcoin", removeScheme = true)
+        val instantTransactionManager = InstantTransactionManager(xcoinStorage, InstantSendFactory(), InstantTransactionState())
+        val initialSyncApi = InsightApi(initialSyncUrl)
+
+        xcoinTransactionInfoConverter = DashTransactionInfoConverter(instantTransactionManager)
+
+        val blockHelper = BlockValidatorHelper(coreStorage)
+
+        val blockValidatorSet = BlockValidatorSet()
+        blockValidatorSet.addBlockValidator(ProofOfWorkValidator())
+
+        val blockValidatorChain = BlockValidatorChain()
+
+        if (network is MainNetXcoin) {
+            blockValidatorChain.add(DarkGravityWaveValidator(blockHelper, heightInterval, targetTimespan, maxTargetBits, 68589))
+        } else {
+            blockValidatorChain.add(DarkGravityWaveTestnetValidator(targetSpacing, targetTimespan, maxTargetBits, 4002))
+            blockValidatorChain.add(DarkGravityWaveValidator(blockHelper, heightInterval, targetTimespan, maxTargetBits, 4002))
+        }
+
+        blockValidatorSet.addBlockValidator(blockValidatorChain)
+
+        bitcoinCore = BitcoinCoreBuilder()
+                .setContext(context)
+                .setSeed(seed)
+                .setNetwork(network)
+                .setPaymentAddressParser(paymentAddressParser)
+                .setPeerSize(peerSize)
+                .setSyncMode(syncMode)
+                .setConfirmationThreshold(confirmationsThreshold)
+                .setStorage(coreStorage)
+                .setBlockHeaderHasher(X11Hasher())
+                .setInitialSyncApi(initialSyncApi)
+                .setTransactionInfoConverter(xcoinTransactionInfoConverter)
+                .setBlockValidator(blockValidatorSet)
+                .build()
+
+        bitcoinCore.listener = this
+
+        //  extending bitcoinCore
+
+
+
+        bitcoinCore.addMessageParser(MasternodeListDiffMessageParser())
+                .addMessageParser(TransactionLockMessageParser())
+                .addMessageParser(TransactionLockVoteMessageParser())
+                .addMessageParser(ISLockMessageParser())
+
+        bitcoinCore.addMessageSerializer(GetMasternodeListDiffMessageSerializer())
+
+        val merkleRootHasher = MerkleRootHasher()
+        val merkleRootCreator = MerkleRootCreator(merkleRootHasher)
+        val masternodeListMerkleRootCalculator = MasternodeListMerkleRootCalculator(merkleRootCreator)
+        val masternodeCbTxHasher = MasternodeCbTxHasher(CoinbaseTransactionSerializer(), merkleRootHasher)
+
+        val quorumListManager = QuorumListManager(xcoinStorage, QuorumListMerkleRootCalculator(merkleRootCreator), QuorumSortedList())
+        val masternodeListManager = MasternodeListManager(xcoinStorage, masternodeListMerkleRootCalculator, masternodeCbTxHasher, MerkleBranch(), MasternodeSortedList(), quorumListManager)
+        val masternodeSyncer = MasternodeListSyncer(bitcoinCore, PeerTaskFactory(), masternodeListManager, bitcoinCore.initialBlockDownload)
+
+        bitcoinCore.addPeerTaskHandler(masternodeSyncer)
+        bitcoinCore.addPeerSyncListener(masternodeSyncer)
+        bitcoinCore.addPeerGroupListener(masternodeSyncer)
+
+        val base58AddressConverter = Base58AddressConverter(network.addressVersion, network.addressScriptVersion)
+        bitcoinCore.addRestoreKeyConverter(Bip44RestoreKeyConverter(base58AddressConverter))
+
+        val singleHasher = SingleSha256Hasher()
+        val bls = BLS()
+        val transactionLockVoteValidator = TransactionLockVoteValidator(xcoinStorage, singleHasher, bls)
+        val instantSendLockValidator = InstantSendLockValidator(quorumListManager, bls)
+
+        val transactionLockVoteManager = TransactionLockVoteManager(transactionLockVoteValidator)
+        val instantSendLockManager = InstantSendLockManager(instantSendLockValidator)
+
+        val instantSendLockHandler = InstantSendLockHandler(instantTransactionManager, instantSendLockManager)
+        instantSendLockHandler.delegate = this
+        val transactionLockVoteHandler = TransactionLockVoteHandler(instantTransactionManager, transactionLockVoteManager)
+        transactionLockVoteHandler.delegate = this
+
+        val instantSend = InstantSend(bitcoinCore.transactionSyncer, transactionLockVoteHandler, instantSendLockHandler)
+        this.instantSend = instantSend
+
+        bitcoinCore.addInventoryItemsHandler(instantSend)
+        bitcoinCore.addPeerTaskHandler(instantSend)
+
+        val calculator = TransactionSizeCalculator()
+        val confirmedUnspentOutputProvider = ConfirmedUnspentOutputProvider(coreStorage, confirmationsThreshold)
+        bitcoinCore.prependUnspentOutputSelector(UnspentOutputSelector(calculator, confirmedUnspentOutputProvider))
+        bitcoinCore.prependUnspentOutputSelector(UnspentOutputSelectorSingleNoChange(calculator, confirmedUnspentOutputProvider))
+    }
+
+    fun xcoinTransactions(fromUid: String? = null, limit: Int? = null): Single<List<DashTransactionInfo>> {
+        return transactions(fromUid, limit).map {
+            it.mapNotNull { it as? DashTransactionInfo }
+        }
+    }
+
+    fun getXcoinTransaction(hash: String): DashTransactionInfo? {
+        return getTransaction(hash) as? DashTransactionInfo
+    }
+
+    // BitcoinCore.Listener
+    override fun onTransactionsUpdate(inserted: List<TransactionInfo>, updated: List<TransactionInfo>) {
+        // check for all new transactions if it's has instant lock
+        inserted.map { it.transactionHash.hexToByteArray().reversedArray() }.forEach {
+            instantSend.handle(it)
+        }
+
+        listener?.onTransactionsUpdate(inserted.mapNotNull { it as? DashTransactionInfo }, updated.mapNotNull { it as? DashTransactionInfo })
+    }
+
+    override fun onTransactionsDelete(hashes: List<String>) {
+        listener?.onTransactionsDelete(hashes)
+    }
+
+    override fun onBalanceUpdate(balance: BalanceInfo) {
+        listener?.onBalanceUpdate(balance)
+    }
+
+    override fun onLastBlockInfoUpdate(blockInfo: BlockInfo) {
+        listener?.onLastBlockInfoUpdate(blockInfo)
+    }
+
+    override fun onKitStateUpdate(state: BitcoinCore.KitState) {
+        listener?.onKitStateUpdate(state)
+    }
+
+    // IInstantTransactionDelegate
+    override fun onUpdateInstant(transactionHash: ByteArray) {
+        val transaction = xcoinStorage.getFullTransactionInfo(transactionHash) ?: return
+        val transactionInfo = xcoinTransactionInfoConverter.transactionInfo(transaction)
+
+        bitcoinCore.listenerExecutor.execute {
+            listener?.onTransactionsUpdate(listOf(), listOf(transactionInfo))
+        }
+    }
+
+    companion object {
+        const val maxTargetBits: Long = 0x1e0fffff
+
+        const val targetSpacing = 150             // 2.5 min. for mining 1 Block
+        const val targetTimespan = 3600L          // 1 hour for 24 blocks
+        const val heightInterval = targetTimespan / targetSpacing
+
+        private fun getDatabaseNameCore(networkType: NetworkType, walletId: String, syncMode: SyncMode) =
+                "${getDatabaseName(networkType, walletId, syncMode)}-core"
+
+        private fun getDatabaseName(networkType: NetworkType, walletId: String, syncMode: SyncMode) =
+                "Xcoin-${networkType.name}-$walletId-${syncMode.javaClass.simpleName}"
+
+        fun clear(context: Context, networkType: NetworkType, walletId: String) {
+            for (syncMode in listOf(SyncMode.Api(), SyncMode.Full(), SyncMode.NewWallet())) {
+                try {
+                    SQLiteDatabase.deleteDatabase(context.getDatabasePath(getDatabaseNameCore(networkType, walletId, syncMode)))
+                    SQLiteDatabase.deleteDatabase(context.getDatabasePath(getDatabaseName(networkType, walletId, syncMode)))
+                } catch (ex: Exception) {
+                    continue
+                }
+            }
+        }
+    }
+
+}
